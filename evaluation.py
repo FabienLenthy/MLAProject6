evaluation.py - evaluation metrics for Forest-RI hopefully correct 	- Computing variance also. Only 3 iterations per dataset to be faster. 	- Saving or loading (pickle) forests and results to files on the go. 	- Out of bag error for trees and forests. rf.py - facilitate computation of out of bag metrics.

evaluation.py
@@ -3,6 +3,7 @@ from datasets import chooseData
 from rf import RandomForest, TreeLeaf
 import numpy as np
 import time
+import pickle
 
 def classify(tree, sample):
     "Classify a sample using the given decition tree"
@@ -35,68 +36,127 @@ def checkForest(forest, testdata):
             correct += 1
     return float(correct)/len(testdata)
 
-ITERATE = 10
-# TODO: Try less iterations (3) and report variance, too.
-# TODO: Save the reported data into a data structure.
-# TODO: Report as a latex table from the data structure.
-# TODO: Pickle the forests! 
-
-def checkOnData(data):
-    t0 = time.time()
-    S = chooseData(data)
-    N = len(S)
-    cumulErrorSelection = 0
-    cumulErrorF1 = 0
-    cumulErrorF2 = 0
-    cumulSpecificError = 0
-
-    # FOR OOB
-    cumulErrorSelectionOOB = 0
-    cumulErrorF1OOB = 0
-    cumulErrorF2OOB = 0
+def metricsRI(oobForest1, oobForest2, oobTree1, oobTree2, errors1, errors2):
+    """ Compute selection, singleInput and onetree metrics for a set of N forests."""
+    N = len(oobForest1)
+    selection = np.zeros(N)
+    singleInput = np.zeros(N)
+    onetree = np.zeros(N)
     
-    for i in range(ITERATE):
-        nbrTrees = 100  # TODO: This number is dataset dependent! (zip-code 200)
-        np.random.shuffle(S)
-        train, test = S[:int(0.9*N)],S[int(0.9*N):]
-        H, S_OOB = RandomForest(train, nbrTrees) # Modified to return OOB samples
-        H2, S_OOB2 = RandomForest(train, nbrTrees, SF=False) # Modified to return OOB samples
-        # Single tree: This should be out-of-bag, not test. Also the best one from H or H2.
-        cumulSpecificError += sum([1-check(h,test) for h in H])/nbrTrees    
-        errorF1 = 1-checkForest(H,test)
-        errorF2 = 1-checkForest(H2,test)
-        # Choose by means of the lowest out-of-bag error. Report the test error.
-        cumulErrorSelection += min(errorF1,errorF2)  
-        cumulErrorF1 += errorF1     # OK.
-
-        # OOB ERROR
-        errorOOB1 = 1-checkForest(H, S_OOB)
-        errorOOB2 = 1-checkForest(H2, S_OOB2)
-        cumulErrorSelectionOOB += min(errorOOB1, errorOOB2)
-        cumulErrorF1OOB += errorOOB1
-        cumulErrorF2OOB += errorOOB2
-    # FINAL OOB ERROR
-    finalErrorOOB1 = cumulErrorF1OOB/ITERATE
-    finalErrorOOB2 = cumulErrorF2OOB/ITERATE
-    finalErrorSelectionOOB = cumulErrorSelectionOOB/ITERATE
+    for i in range(N):
+        flag = 0
+        # 'single input': test error, just 1 random feature to grow the trees
+        singleInput[i] = errors1[i]
+        
+        # 'one tree': out-of-bag, averaged over individual trees, for the best setting (single or selection)
+        onetree[i] = oobTree1[i]
+        
+        # The 'selection' result: 
+        #   Choose the best forest based on the lowest out-of-bag error of that forest.
+        if(oobForest1[i] < oobForest2[i]):
+            selection[i] = errors1[i]
+        else:
+            selection[i] = errors2[i]
+            if(selection[i] < singleInput[i]):
+                onetree[i] = oobTree2[i]
     
-    #   
-    finalErrorF1 = cumulErrorF1/ITERATE
-    finalErrorSelection = cumulErrorSelection/ITERATE
-    finalSpecificError = cumulSpecificError/ITERATE
-    
+    return [selection, singleInput, onetree]
+        
+def reportRI(selection, singleInput, onetree, data, S, runtime):
+    mSel = np.mean(selection)
+    mSingle = np.mean(singleInput)
+    mOne = np.mean(onetree)
+    stdSel = np.std(selection)
+    stdSingle = np.std(singleInput)
+    stdOne = np.std(onetree)
     print("Data :", data)
     print("Number of input :", str(S[0].getNbrAttributes()))
     print("Number of data point :", str(len(S)))
-    print("Error rate with selection :", str(finalErrorSelection))
-    print("Error rate with single input :", str(finalErrorF1))
-    print("Error rate with individual trees :", str(finalSpecificError))
-    print('Execution time: {} seconds.'.format(time.time() - t0))
+    print("Error rate with selection : {} +- {}".format(mSel, stdSel))
+    print("Error rate with single input : {} +- {}".format(mSingle, stdSingle))
+    print("Error rate with individual trees :{} +- {}".format(mOne, stdOne))
+    print('Execution time: {} seconds.'.format(runtime))
     print()
-    #OOB PRINTS
-    print('H OOB :', str(finalErrorOOB1))
-    print('H2 OOB :', str(finalErrorOOB2))
-    print('H OOB selection :', str(finalErrorSelectionOOB))
+    
+
+def outOfBagError(H, Sb, train, param = 'forest'):
+    N = len(H)
+    oob = 0.0
+    if param == 'tree':
+        for i in range(N):
+            oob += 1-check(H[i], list(Sb[i]))
+        return oob/N
+    elif param == 'forest':
+        for s in train:
+            Hm = []
+            # Build a smaller forest and classify with it.
+            for i in range(N):
+                if s in Sb[i]:
+                    Hm.append(H[i])
+            assert(len(Hm) > 2) # This might fail but very rarely.
+            oob += 1-checkForest(Hm, [s])
+        return oob/len(train)
+    else:
+        raise('outOfBagError: unexpected parameter ' + param)
+        return None
+    
+    
+def save(ar, fname):
+    f = open(fname, 'wb')
+    pickle.dump(ar, f)
+    f.close()
+    
+def load(fname):
+    f = open(fname, 'rb')
+    ar = pickle.load(f)
+    f.close()
+    return ar
+
+ITERATE = 3
+# We 'should' run 100 iterations over everything on the first 10 'small' datasets like in the paper! :-)
+# TODO: Report as a latex table from the data structure.
+
+def checkOnData(data, training = True, evaluating = True):
+    t0 = time.time()
+    modelPrefix = 'models/RI/'
+    resultPrefix = 'results/RI/'
+    S = chooseData(data)
+    N = len(S)
+    # Test and out-of-bag errors.
+    errors1 = np.zeros(ITERATE)
+    errors2 = np.zeros(ITERATE)
+    oobTree1 = np.zeros(ITERATE)
+    oobTree2 = np.zeros(ITERATE)
+    oobForest1 = np.zeros(ITERATE)
+    oobForest2 = np.zeros(ITERATE)
+    for i in range(ITERATE):
+        nbrTrees = 100  # TODO: This number is dataset dependent! (zip-code 200)
+        if training:
+            np.random.shuffle(S)
+            train, test = S[:int(0.9*N)],S[int(0.9*N):]
+            [H, Sb] = RandomForest(train, nbrTrees)
+            [H2, Sb2] = RandomForest(train, nbrTrees, SF=False)
+            save([[H, Sb, H2, Sb2], train, test], modelPrefix + data + 'Forests' + str(i) + '.pkl') 
+        elif evaluating:
+            [[H, Sb, H2, Sb2], train, test] = load(modelPrefix + data + 'Forests' + str(i) + '.pkl')
+            nbrTrees = len(H)
+            
+        if evaluating:
+            oobForest1[i] = outOfBagError(H, Sb, train, 'forest')
+            oobForest2[i] = outOfBagError(H2, Sb2, train, 'forest')
+            oobTree1[i] = outOfBagError(H, Sb, train, 'tree')
+            oobTree2[i] = outOfBagError(H2, Sb2, train, 'tree')
+            errors1[i] =  1-checkForest(H,test)
+            errors2[i] =  1-checkForest(H2,test)
+    runtime = time.time() - t0
+    if not evaluating:
+        [oobForest1, oobForest2, oobTree1, oobTree2, errors1, errors2, runtime] = load(resultPrefix + data + 'Results.pkl')
+    else:
+        save([oobForest1, oobForest2, oobTree1, oobTree2, errors1, errors2, runtime], resultPrefix + data + 'Results.pkl') 
+    
+    [selection, singleInput, onetree] = metricsRI(oobForest1, oobForest2, oobTree1, oobTree2, errors1, errors2)
+    reportRI(selection, singleInput, onetree, data, S, runtime)
+    
     
 if __name__=="__main__":
     checkOnData("glass")

rf.py
@@ -36,18 +36,14 @@ def RandomForest(S, nbrTrees = 100, F=None, SF=True):
         F = range(S[0].getNbrAttributes())
     B = nbrTrees                    # number of trees in forest
     H = []
+    Sb = []
+    Sset = set(S)
     for i in range(B):
-        S_i = BootstrapSample(S)
-
-        # We need the out-of-bag samples for OOB estimate
-        S_OOB = []
-        for i in S:
-            if i not in S_i:
-                S_OOB.append(i)
-        
+        [S_i, Stst] = BootstrapSample(S, Sset)
         h_i = RandomizedTreeLearn(S_i, F, SF)
         H.append(h_i)
-    return H, S_OOB
+        Sb.append(Stst)
+    return [H, Sb]
 
 def RandomizedTreeLearn(S, F, singleFeature, maxdepth = MAXDEPTH): # here S is a subsample 
     def buildBranch(dataset, default, attributes):
@@ -72,9 +68,11 @@ def RandomizedTreeLearn(S, F, singleFeature, maxdepth = MAXDEPTH): # here S is a
                 buildBranch(dataR, default, attributesLeftR)]
     return TreeNode(a, branches, default, v)
 
-def BootstrapSample(S):
+def BootstrapSample(S, Sset):
     soS = int(len(S) * 2/3)    # size of subsample
-    return np.random.choice(S, soS, replace = True).tolist()
+    train = np.random.choice(S, soS, replace = True).tolist()
+    tst = Sset.difference(set(train))
+    return [train, tst] 
 
 def FeatureSubsample(F, singleFeature):
     soS = soF